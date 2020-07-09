7db771557d212f36f97ca0accca43348a1e60a58
Santhiya <santhiyaps@gmail.com>
2020-07-08 20:38:57 -0700

    Two Sum Time:O(nlogn) Space:O(1)

---
+++ TwoSum1.java
@@ -0,0 +1,25 @@
+public class TwoSum1 {
+
+    public int[] twoSum(int[] numbers, int target) {
+        int []result=new int[2];
+        int left=0;
+        int right=numbers.length-1;
+        while(left<right){
+            int currSum=numbers[left]+numbers[right];
+            if(target==currSum){
+                result[0]=left+1;
+                result[1]=right+1;
+                return result;
+            }else if(target<currSum){
+                right--;
+            } else if(target>currSum){
+                left++;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        new TwoSum1().twoSum(new int[]{2,7,11,15},9);
+    }
+}
